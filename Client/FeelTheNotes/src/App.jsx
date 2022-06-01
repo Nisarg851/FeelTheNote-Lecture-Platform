1eb0fa3d9f40a74fbdb65fcf3a88344114e6cf4a
Nisarg Mahyavanshi <nisargmahyavanshi8501@gmail.com>
2022-06-01 10:03:13 +0530

    MVP HomePage ready!

--- Client/FeelTheNotes/src/App.jsx
+++ Client/FeelTheNotes/src/App.jsx
@@ -7,11 +7,11 @@ import './App.css'
 const App = () => {
   return(
     <Box>
-      <Grid container rowSpacing={{md:16}}>
+      <Grid container rowSpacing={{md:14}}>
         <Grid item xs={12}>
           <MainHeader/> 
         </Grid>
-        <Grid item xs={12} md={6} style={{backgroundColor:"blue"}}>
+        <Grid item xs={12} md={6}>
           <MeetInitiator/>
         </Grid>
         <Grid item xs={12} md={5}>
--- Client/FeelTheNotes/src/components/IndexStartMeetingContainer/MeetInitiator.jsx
+++ Client/FeelTheNotes/src/components/IndexStartMeetingContainer/MeetInitiator.jsx
@@ -1,19 +1,40 @@
 import { Button, Container, Grid, TextField } from "@mui/material";
+import { InputAdornment } from "@mui/material";
+import VideocamIcon from '@mui/icons-material/Videocam';
+import KeyboardRoundedIcon from '@mui/icons-material/KeyboardRounded';
 import "./MeetInitiator.css"
 
 const MeetInitiator = () => {
     return(
-        <Container sx={{width:"80%"}} style={{backgroundColor:"red"}}>
+        <Container sx={{width:"80%"}}>
             <h1> Welcome back,</h1>
             <h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium assumenda at nam ut ex architecto quibusdam nobis aut amet exercitationem illum porro unde minima eius officia corrupti libero, fugiat dignissimos consequuntur maxime? Deserunt, quia. Nihil asperiores unde accusamus consequuntur a corporis, beatae dicta possimus? Voluptates mollitia harum magnam animi?</h4>
-            <Grid container columnSpacing={2} rowSpacing={2}
+            <Grid container 
+                columnSpacing={2}
+                rowSpacing={2}
                 justifyContent="start"
-                alignItems="center">
+                alignItems="center"
+                >
                 <Grid item xs={12} md="auto">
-                    <Button variant="contained" color="primary" sx={{height:"100%"}}>New meeting</Button>
+                    <Button variant="contained" 
+                        color="primary"
+                        sx={{height:"100%"}} 
+                        startIcon={<VideocamIcon/>}>
+                            New meeting
+                    </Button>
                 </Grid>
                 <Grid item xs={12} md="auto">
-                <TextField id="outlined-basic" label="Enter a code or Link" variant="outlined" size="small"/>
+                    <TextField id="outlined-basic" 
+                        label="Enter a code or Link" 
+                        variant="outlined" 
+                        size="small"
+                        InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <KeyboardRoundedIcon />
+                              </InputAdornment>
+                            ),
+                          }}/>
                 </Grid>
                 <Grid item xs={12} md="auto">
                     <Button>Join</Button>
